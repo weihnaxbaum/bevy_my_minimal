ae133f1c71ff8a0294891d6d0065da4a65ead928
Weihnachtsbaum <140716472+Weihnachtsbaum@users.noreply.github.com>
2025-08-16 19:50:31 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+#![cfg_attr(not(feature = "console"), windows_subsystem = "window")]
+
+use bevy::prelude::*;
+
+fn main() -> AppExit {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run()
+}
+
+fn setup(mut commands: Commands) {}
